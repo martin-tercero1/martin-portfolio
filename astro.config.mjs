90b89a12e7dea4bc2ea8128140fbbc91d5305e5a
martin-tercero1 <martintercero20@hotmail.com>
2024-12-09 19:08:33 -0600

    Github Action to deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://martin-tercero.com",
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react(), mdx()]
